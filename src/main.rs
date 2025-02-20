673aad7284aa8476b3e7fa86c68239cfbbbb32b7
lgmarchi <lucasgmarchi@gmail.com>
2025-02-20 14:03:14 -0300

    Update dependencies and implement basic API server with Actix-web

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+mod api;
+
+use api::task::
+    get_task
+;
+
+use actix_web::{HttpServer, App, web::Data, middleware::logger};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "debug");
+    std::env::set_var("RUST_BACKTRACE", "1");
+    env_logger::init();
+
+    HttpServer::new(move || {
+        let logger = Logger::default();
+        App::new()
+        .wrap(logger)
+        .service(get_task)
+    }).bind(("127.0.0.1", 80))?
+    .run()
+    .await
 }
